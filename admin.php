e8180eaff5e197087d18999eb42d0dd32ff798a7
nedh <nedh.design@gmail.com>
2023-03-29 10:24:24 +0200

    creation ossature base

---
+++ admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="inc/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+
+        <div class="container">
+
+            <main>
+
+            </main>
+
+        </div>
+        
+        <div class="push"></div>
+
+    </div>
+</body>
+</html>
