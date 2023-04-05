9b3f1b47d2a818b834d9e50ba8e60c8748baeb66
jeremy <jeremy.nowak@laplateforme.io>
2023-04-05 13:53:41 +0200

    page admin gestion bien avancé

--- inc/class/Product.php
+++ inc/class/Product.php
@@ -1,15 +1,16 @@
 <?php
 require_once "Model.php";
-class Product extends Model{
+class Product extends Model
+{
     protected $bdd;
     protected $tablename = "product";
-    
+
     public function __construct()
     {
-            parent::__construct();
+        parent::__construct();
     }
 
-    public function getAll($categ=null)
+    public function getAll($categ = null)
     {
         $request = "SELECT product.id_product, product.title, product.description, product.image, product.price, product.sales, link_categ.id_product 
         AS id_link_product, link_categ.id_categ, category.id_category, category.name 
@@ -20,8 +21,8 @@ class Product extends Model{
         INNER JOIN category 
         ON link_categ.id_categ=category.id_category";
 
-        if ($categ!=null){
-            $categ=htmlspecialchars($categ);
+        if ($categ != null) {
+            $categ = htmlspecialchars($categ);
             $request = $request . " WHERE category.id_category=$categ";
         }
 
@@ -35,21 +36,22 @@ class Product extends Model{
     }
 
     public function getInfo($table)
-    {   
+    {
         $table = htmlspecialchars($table);
         $this->tablename = $table;
         $result = parent::getAll();
         $this->tablename = "product";
         return $result;
-
     }
 
-    public function deleteProduct($id){
+    public function deleteProduct($id)
+    {
 
         echo parent::deleteProduct($id);
     }
 
-    public function completion() {
+    public function completion()
+    {
 
         $get = $this->bdd->prepare("SELECT id_product, title FROM $this->tablename");
         $get->execute();
@@ -60,22 +62,23 @@ class Product extends Model{
         echo $myJSON;
     }
 
-    public function searchProducts($search) {
+    public function searchProducts($search)
+    {
         $search = htmlspecialchars($search) . "%";
-    
+
         $query = $this->bdd->prepare("SELECT id_product, title FROM product WHERE title LIKE :search LIMIT 0,20;");
         $query->execute([':search' => $search]);
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
-    
+
         // return the search results
         return $result;
     }
 
-    public function getProductInfo($id) {
+    public function getProductInfo($id)
+    {
         $query = $this->bdd->prepare("SELECT * FROM $this->tablename WHERE id_product = :id");
         $query->execute([':id' => $id]);
         $result = $query->fetch(PDO::FETCH_ASSOC);
         return $result;
     }
-    
 }
