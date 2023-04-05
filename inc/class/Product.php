5c2f5c63781d0e65d78a447bf899c767c2900f7e
jeremy <jeremy.nowak@laplateforme.io>
2023-04-05 13:53:41 +0200

    page admin gestion bien avancé

--- inc/class/Product.php
+++ inc/class/Product.php
@@ -11,12 +11,32 @@ class Product extends Model{
 
     public function getAll($categ=null)
     {
-        $request = "SELECT  product.id_product, product.title, product.description, product.image, product.price, product.sales, linkCateg.id_product AS id_link_product, linkCateg.id_categ, category.id_category, category.name FROM $this->tablename  INNER JOIN linkCateg ON product.id_product=linkCateg.id_product INNER JOIN category ON linkCateg.id_categ=category.id_category";
+        $request = "SELECT product.id_product, product.title, product.description, product.image, product.price, product.sales, link_categ.id_product AS id_link_product, link_categ.id_categ, category.id_category, category.name AS category FROM $this->tablename INNER JOIN link_categ ON product.id_product=link_categ.id_product INNER JOIN category ON link_categ.id_categ=category.id_category";
 
         if ($categ!=null){
+            $categ=htmlspecialchars($categ);
             $request = $request . " WHERE category.id_category=$categ";
         }
+
+        $select = $this->bdd->prepare($request);
+
+        $select->execute();
+
+        $result = $select->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
     }
+
+    public function getInfo($table)
+    {   
+        $table = htmlspecialchars($table);
+        $this->tablename = $table;
+        $result = parent::getAll();
+        $this->tablename = "product";
+        return $result;
+
+    }
+
 
 }   
 
--- inc/php/adminGestion.php
+++ inc/php/adminGestion.php
@@ -62,7 +62,6 @@ elseif(isset($_GET["products"])):
     require_once "../class/Product.php";
     $product = new Product();
     $products = $product->getAll();
-    var_dump($products);
 ?>
     <table>
         <thead>
@@ -93,7 +92,9 @@ elseif(isset($_GET["products"])):
             <tr>
                 <th> <span>cross</span> <span>crayon</span> <?= $value['title'] ?></th>
                 <th><?= $value['id_product']?></th>
-                <th><?= "yo" ?></th>
+                <th><?= $value['category']?></th>
+                <th><?= "5"?></th>
+                <th><?= $value['description']?></th>
             </tr>
 
         <?php
@@ -103,6 +104,110 @@ elseif(isset($_GET["products"])):
         </tbody>
     </table>
 
+    <?php 
+elseif(isset($_GET["addProducts"])):
+    require_once "../class/Product.php";
+    $product = new Product();
+    $tableSize = 'size';
+    $tableCategory = 'category';
+    $sizes = $product->getInfo($tableSize);
+    $categories = $product->getInfo($tableCategory);
+?>
+
+<form method="POST" id="addProduct">
+
+    <label for="title">Title</label>
+    <input type="text" name="title">
+
+    <label for="descritpion">Description</label>
+    <input type="text" name="description">
+
+    <label for="image">photo</label>
+    <input type="file" name="image">
+
+    <label for="category">Category</label>
+    <select name="category">
+        <option value="" disabled selected>Category</option>
+        <?php  
+        foreach($categories as $cols=>$value){
+            ?>
+            <option value="<?= $value['id_category'] ?>"><?= $value['name']?></option>
+            <?php
+        }
+        ?>
+    </select>
+
+    <div class="size">
+    <label for="size">Size</label>
+    <select name="size">
+        <option value="" disabled selected>Size</option>
+        <?php  
+        foreach($sizes as $cols=>$value){
+            ?>
+            <option value="<?= $value['id_size'] ?>"><?= $value['size']?></option>
+            <?php
+        }
+        ?>
+    </select>
+
+    <button id="addSize"><span>+</span> </button> add a new size
+    </div>
+
+    <label for="stock">Stock</label>
+    <input type="number" name="stock" min="1">
+
+    <label for="price">Price</label>
+    <input type="number" name="price" min="1"id="euros">
+    <span>€</span>
+    <input type="number" name="price" min="0" id="centimes">
+
+    <input type="submit" name="add" value="ADD">
+</form>
+
+<?php 
+elseif(isset($_GET["categories"])):
+    require_once "../class/Product.php";
+    $product = new Product();
+    $tableCategory = 'category';
+    $categories = $product->getInfo($tableCategory);
+?>
+<table>
+    <thead>
+        <tr>
+            <th>Manage</th>
+            <th>CATEGORY</th>
+        </tr>
+
+    </thead>
+    <tbody>
+        <?php
+            foreach($categories as $col=>$value){
+
+            
+        ?>
+        <tr>
+            <th>
+                <span>Dustbin</span> <span>Pencil</span> 
+            </th>
+            <th>
+                <?= $value['name'] ?>
+            </th>
+        </tr>
+        <?php } ?>
+    </tbody>
+
+
+</table>
+<?php 
+elseif(isset($_GET["addCategories"])):
+
+?>
+
+<form method="POST" id="addCategory">
+    <label for="category">Add a category</label>
+    <input type="text">
+    <button id="validate">ADD</button>
+</form>
 
 <?php
 endif;
