f96e09513cace737f6e751825104a5b5874c3f79
jeremy <jeremy.nowak@laplateforme.io>
2023-04-19 16:05:16 +0200

    cart quasiment terminé

--- inc/class/Cart.php
+++ inc/class/Cart.php
@@ -78,11 +78,12 @@ class Cart extends Model{
         $size = htmlspecialchars($size);
         $total = htmlspecialchars($total);
 
-        $request = "SELECT * FROM detail WHERE id_order = :id_order AND id_product = :id_product";
+        $request = "SELECT * FROM detail WHERE id_order = :id_order AND id_product = :id_product AND size = :size";
         $select = $this->bdd->prepare($request);
         $select->execute([
             ":id_order"=>$id_order,
-            ":id_product"=>$id_product
+            ":id_product"=>$id_product,
+            ":size"=>$size,
         ]);
         $result = $select->fetch(PDO::FETCH_ASSOC);
         if($result){
@@ -90,13 +91,14 @@ class Cart extends Model{
             $quantity = $quantity + $result["quantity"];
             $total = $total + $result['total'];
 
-            $request2 = "UPDATE detail SET total=:total, quantity=:quantity WHERE id_order = :id_order AND id_product = :id_product";
+            $request2 = "UPDATE detail SET total=:total, quantity=:quantity WHERE id_order = :id_order AND id_product = :id_product AND size = :size";
             $update = $this->bdd->prepare($request2);
             $update->execute([
                 ":total"=>$total,
                 ":quantity"=>$quantity,
                 ":id_order"=>$id_order,
-                "id_product"=>$id_product
+                "id_product"=>$id_product,
+                ":size"=>$size,
             ]);
             if($update){
                 return "ok";
@@ -162,6 +164,51 @@ class Cart extends Model{
     }
 
 
+    public function deleteProduct($id_product, $id_order, $size){
+
+        $request = "DELETE FROM detail WHERE id_product = :id_product AND id_order = :id_order AND size = :size"; 
+        $delete = $this->bdd->prepare($request);
+        $delete->execute([
+            ":id_product"=>$id_product,
+            "id_order"=>$id_order,
+            ":size"=>$size,
+        ]);
+
+        if($delete){
+            return "ok";
+        }
+        else{
+            return "error";
+        }
+    }
+    
+    public function updateQuantity($id_product, $size_order, $id_order, $quantity, $total){
+
+        $id_product = htmlspecialchars($id_product);
+        $size_order = htmlspecialchars($size_order);
+        $id_order = htmlspecialchars($id_order);
+        $quantity = htmlspecialchars($quantity);
+        $total = htmlspecialchars($total);
+
+
+        $request = "UPDATE detail SET quantity = :quantity ,total = :total WHERE id_product = :id_product AND size = :size AND id_order = :id_order";
+
+        $update = $this->bdd->prepare($request);
+
+        $update->execute([
+            ":quantity" => $quantity,
+            ":total" => $total,
+            ":id_product" => $id_product,
+            ":size" => $size_order,
+            "id_order" => $id_order
+        ]);
+        if($update){
+            return "ok";
+        }
+        else{
+            return "error";
+        }
+    }
 
 
 
--- inc/php/process-order.php
+++ inc/php/process-order.php
@@ -26,7 +26,7 @@ if(isset($_GET["cart"]))
     <div class="d-flex justify-content-between align-items-center mb-4">
         <div>
             <p class="mb-1">Shopping cart</p>
-            <p class="mb-0">You have <?= $count ?> items in your cart</p>
+            <p class="mb-0">You have <?= $count ?> different items in your cart</p>
         </div>
     </div>
 <?php
@@ -34,14 +34,14 @@ if(isset($_GET["cart"]))
         die();
     }
     foreach ($carts as $order ) {
-
         $id_pro = $order['id_product'];
         $item = $product->getProductInfo($id_pro);
-        $size = $order["size"];   
-
+        $size = $order["size"];      
+        $id_order = $cart->cartVerify($id);
+  
         ?>
         <div class="card mb-3">
-            <div class="card-body">
+            <div class="card-body shadow p-3 bg-white rounded">
                 <div class="d-flex justify-content-between">
                     <div class="d-flex flex-row align-items-center">
                         <div>
@@ -58,7 +58,7 @@ if(isset($_GET["cart"]))
                     <div class="d-flex flex-row align-items-center">
                         <div style="width: 50px;">
                             <div class="form-outline">
-                                <input id="form1" min="0" name="quantity" value="<?=$order['quantity']?>" type="number" class="form-control" />
+                                <input id="form1" min="1" name="quantity" value="<?=$order['quantity']?>" type="number" class="quantity form-control" data-id="<?=$id_pro?>" data-size="<?= $size?>" data-order="<?= $id_order?>"/>
                             </div>
                         </div>
                     </div>
@@ -68,7 +68,7 @@ if(isset($_GET["cart"]))
                         </div>
                     </div>
                     <div class="d-flex flex-row align-items-center">
-                        <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
+                        <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt" data-id="<?=$id_pro?>" data-size="<?= $size?>" data-order="<?= $id_order?>" id="delProduct" ></i></a>
                     </div>
                 </div>
             </div>
@@ -78,7 +78,88 @@ if(isset($_GET["cart"]))
     }
 }
 
+if(isset($_GET["pay"]))
+{
+    $id = $user->getUserId();
+    $total = $cart->getCart($id);
+?>
+    <div class="card bg-dark text-white rounded-3">
+        <div class="card-body">
+            <div class="d-flex justify-content-between align-items-center mb-4">
+                <h5 class="mb-0">Card details</h5>
+                <h6>User</h6>
+            </div>
+
+            <p class="small mb-2">Card type</p>
+            <a href="#!" type="submit" class="text-white"><i
+                class="fab fa-cc-mastercard fa-2x me-2"></i></a>
+            <a href="#!" type="submit" class="text-white"><i
+                class="fab fa-cc-visa fa-2x me-2"></i></a>
+            <a href="#!" type="submit" class="text-white"><i
+                class="fab fa-cc-amex fa-2x me-2"></i></a>
+            <a href="#!" type="submit" class="text-white"><i class="fab fa-cc-paypal fa-2x"></i></a>
+
+            <form class="mt-4">
+                <div class="form-outline form-white mb-4">
+                    <input type="text" id="typeName" class="form-control form-control-lg" siez="17"
+                    placeholder="Cardholder's Name" />
+                    <label class="form-label" for="typeName">Cardholder's Name</label>
+                </div>
+
+                <div class="form-outline form-white mb-4">
+                    <input type="text" id="typeText" class="form-control form-control-lg" siez="17"
+                    placeholder="1234 5678 9012 3457" minlength="19" maxlength="19" />
+                    <label class="form-label" for="typeText">Card Number</label>
+                </div>
+
+                <div class="row mb-4">
+                    <div class="col-md-6">
+                        <div class="form-outline form-white">
+                            <input type="text" id="typeExp" class="form-control form-control-lg"
+                            placeholder="MM/YYYY" size="7" id="exp" minlength="7" maxlength="7" />
+                            <label class="form-label" for="typeExp">Expiration</label>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-outline form-white">
+                            <input type="password" id="typeText" class="form-control form-control-lg"
+                                placeholder="&#9679;&#9679;&#9679;" size="1" minlength="3" maxlength="3" />
+                            <label class="form-label" for="typeText">Cvv</label>
+                        </div>
+                    </div>
+                </div>
+            </form>
+
+            <hr class="my-4">
+
+            <div class="d-flex justify-content-between">
+                <p class="mb-2">Subtotal</p>
+                <p class="mb-2"><?= $total[0]['total']?>€</p>
+            </div>
+
+            <div class="d-flex justify-content-between">
+                <p class="mb-2">Shipping</p>
+                <p class="mb-2">00.00€</p>
+            </div>
+
+            <div class="d-flex justify-content-between mb-4">
+                <p class="mb-2">Total(Incl. taxes)</p>
+                <p class="mb-2"><?= $total[0]['total']?>€</p>
+            </div>
+
+            <button type="button" class="btn btn-info btn-block btn-lg">
+                <div class="d-flex justify-content-between">
+                    <span><?= $total[0]['total']?>€ </span>
+                    <span>&nbsp; Checkout <i class="fas fa-long-arrow-alt-right ms-2"></i></span>
+                </div>
+            </button>
+        </div>
+    </div>
+<?php
+}
+
 if(isset($_POST["addToCart"])){
+
     $id_product = $_POST["id"];
     $quantity = $_POST["quantity"];
     $size = $_POST["size"];
@@ -95,6 +176,37 @@ if(isset($_POST["addToCart"])){
     } ;
 }
 
+if(isset($_POST["delFromCart"])){
+
+    $id_product = $_POST["id"];
+    $size = $_POST["size"];
+    $id_order = $_POST["id_order"];
+
+    $result = $cart->deleteProduct($id_product, $id_order, $size);
+    if ($result === "ok"){
+        $cart->updateTotal($id_order);
+    }
+
+}
+
+if(isset($_POST["updateProduct"])){
+
+    $id_product = $_POST["id"];
+    $size_product = $_POST["size"];
+    $id_order = $_POST["id_order"];
+    $quantity = $_POST["quantity"];
+
+    $item = $product->getProductInfo($id_product);
+    $total = $item["price"]*$quantity;
+
+
+    $result = $cart->updateQuantity($id_product, $size_product, $id_order, $quantity, $total);
+
+    if ($result === "ok"){
+        $cart->updateTotal($id_order);
+    }
+
+}
 
 
 
